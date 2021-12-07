602eb2a021b1ea064a53a82ac42458fb15c22459
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-07 10:35:27 -0800

    Pass fourth test
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -3,13 +3,22 @@ require('pry')
 class Word
   def initialize(num)
     @num=num
-    @number_words = {1 => "one"}
+    @number_words = {1 => "one", 2=>"two", 3=>"three", 4=>"four", 5=>"five",6=>"six",7=>"seven",8=>"eight",9=>"nine",10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",17=>"seventeen",18=>"eighteen",19=>"nineteen",20=>"twenty"}
+    @tens_words = {2 => "twenty" , 3=>"thirty", 4=>"fourty", 5=>"fifty", 6=>"sixty", 7=>"seventy", 8=>"eighty", 9=>"ninty",}
+    @word_array = []
+    @split_num = []
   end
   def to_word
-    @number_words.fetch(1)
+    if @num<=20
+      return @number_words.fetch(@num)
+    else
+      @split_num = @num.to_s.split("").reverse()
+        @word_array.unshift(@number_words.fetch(@split_num[0].to_i))
+        @word_array.unshift(@tens_words.fetch(@split_num[1].to_i))
+    end
+    @word_array.join(" ")
   end
 end
-
 
 
 
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,8 +3,20 @@ require('numbers_to_words')
 
 describe('Word#to_word') do
 word_1 = Word.new(1)
+word_2 = Word.new(18)
+word_3 = Word.new(25)
+word_4 = Word.new(66)
   it("returns one when 1 is entered") do
     expect(word_1.to_word).to(eq("one"))
+  end
+  it("returns eighteen when 18 is entered") do
+    expect(word_2.to_word).to(eq("eighteen"))
+  end
+  it("returns twentyfive when 25 is entered") do
+    expect(word_3.to_word).to(eq("twenty five"))
+  end
+  it("returns sixty six when 66 is entered") do
+    expect(word_4.to_word).to(eq("sixty six"))
   end
 end
 
